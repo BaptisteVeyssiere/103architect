8d6d3ddf3cebf13c39ed44978f5c44fccc35f705
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2015-11-30 16:03:22 +0100

    com

--- case.c
+++ case.c
@@ -5,7 +5,7 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Mon Nov 30 14:12:06 2015 Baptiste veyssiere
-** Last update Mon Nov 30 15:04:09 2015 Baptiste veyssiere
+** Last update Mon Nov 30 16:02:45 2015 Baptiste veyssiere
 */
 
 #include <stdio.h>
@@ -16,28 +16,49 @@ double	asin(double);
 
 void	case_t(double x, double y, double Tx, double Ty)
 {
-  printf("Translation by the vector (%.f, %.f)\n1.000       0.000     %.3f\n0.000       1.000     %.3f\n0.000       0.000     1.000\n(%.f,%.f) => (%.f,%.f)\n", Tx, Ty, Tx, Ty, x, y, Tx + x, Ty + y);
+  printf("Translation by the vector (%.3g, %.3g)\n1.000       0.000     %.3f\n0.000       1.000     %.3f\n0.000       0.000     1.000\n(%.3g,%.3g) => (%.3g,%.3g)\n", Tx, Ty, Tx, Ty, x, y, Tx + x, Ty + y);
 }
 
 void    case_h(double x, double y, double Tx, double Ty)
 {
-  printf("Homothety by the ratios %.f and %.f\n%.3f      0.000     0.000\n0.000       %.3f     0.000\n0.000       0.000     1.000\n(%.f,%.f) => (%.f,%.f)\n", Tx, Ty, Tx, Ty, x, y, Tx * x, Ty * y);
+  printf("Homothety by the ratios %.3g and %.3g\n%.3f    0.000      0.000\n0.000     %.3f      0.000\n0.000     0.000      1.000\n(%.3g,%.3g) => (%.3g,%.3g)\n", Tx, Ty, Tx, Ty, x, y, Tx * x, Ty * y);
 }
 
 void    case_r(double x, double y, double A, double buffer)
 {
-  double	a = acos(A);
-  double	b = -asin(A);
-  double	c = asin(A);
-  double	d = acos(A);
-  printf("Rotation at a %.f degree angle\n%.3f       %.3f     0.000\n%.3f       %.3f     0.000\n0.000       0.000     1.000\n(%.f,%.f) => (%.f,%.f)\n", x, y, a, b, c, d, x, y, a * x + b * y, c * x + d * y);
+  double	a = cos((M_PI * A) / 180);
+  double	b = -sin((M_PI * A) / 180);
+  double	c = sin((M_PI * A) / 180);
+  double	d = cos((M_PI * A) / 180);
+  if (a < 0.001)
+    a = 0;
+  if (b < 0.001)
+    b = 0;
+  if (c < 0.001)
+    c = 0;
+  if (d < 0.001)
+    d = 0;
+  printf("Rotation at a %.3g degree angle\n%.3f       %.3f     0.000\n%.3f       %.3f     0.000\n0.000       0.000     1.000\n(%.3g,%.3g) => (%.3g,%.3g)\n", A, a, b, c, d, x, y, (a * x + b * y), (c * x + d * y));
 }
 
 void    case_s(double x, double y, double A, double buffer)
 {
-  double	a = acos(2 * A);
-  double	b = asin(2 * A);
-  double	c = asin(2 * A);
-  double	d = -acos(2 * A);
-  printf("Symmetry about an axis inclined with an angle of %.f degrees\n%3f       %3f     0.000\n%3f       %3f     0.000\n0.000       0.000     1.000\n(%.f,%.f) => (%.f,%.f)\n", x, y, a, b, c, d, x, y, a * x + b * y);
+  double	a = cos(2 * (M_PI * A) / 180);
+  double	b = sin(2 * (M_PI * A) / 180);
+  double	c = sin(2 * (M_PI * A) / 180);
+  double	d = -cos(2 * (M_PI * A) / 180);
+  if (a < 0.001)
+    a = 0;
+  if (b < 0.001)
+    b = 0;
+  if (c < 0.001)
+    c = 0;
+  if (d < 0.001)
+    d = 0;
+  printf("Symmetry about an axis inclined with an angle of %.3g degrees\n%.3f       %.3f     0.000\n%.3f       %.3f     0.000\n0.000       0.000     1.000\n(%.3g,%.3g) => (%.3g,%.3g)\n", A, a, b, c, d, x, y, (a * x + b * y), (c * x + d * y));
+}
+
+void	main()
+{
+  case_r(1, 0, 90, 0);
 }
