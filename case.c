b42b3ed33373a6b5e1799b0055b5403ee5c21cba
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2015-12-02 14:04:45 +0100

    com

--- case.c
+++ case.c
@@ -6,7 +6,7 @@
 **
 ** Started on  Mon Nov 30 14:12:06 2015 Baptiste veyssiere
 <<<<<<< HEAD
-** Last update Mon Nov 30 18:25:26 2015 Baptiste veyssiere
+** Last update Wed Dec  2 14:04:04 2015 Baptiste veyssiere
 =======
 ** Last update Mon Nov 30 16:58:37 2015 Baptiste veyssiere
 >>>>>>> 0e0be9e27d650587c3d0e8b87a49e7f744c0bcb2
@@ -20,12 +20,12 @@ void	case_t(double x, double y, double Tx, double Ty, t_matrix *mat)
 {
   printf("%.3g %.3g\n", mat->x, mat->y);
   printf("Translation by the vector (%.3g, %.3g)\n", Tx, Ty);
-  mat->tab[0][0] += 1;
-  mat->tab[0][1] += 0;
-  mat->tab[0][2] += Tx;
-  mat->tab[1][0] += 0;
-  mat->tab[1][1] += 1;
-  mat->tab[1][2] += Ty;
+  mat->tab[0][0] *= 1;
+  mat->tab[0][1] *= 0;
+  mat->tab[0][2] *= Tx;
+  mat->tab[1][0] *= 0;
+  mat->tab[1][1] *= 1;
+  mat->tab[1][2] *= Ty;
   mat->x = Tx + mat->x;
   mat->y = Ty + mat->y;
 }
@@ -34,12 +34,12 @@ void    case_h(double x, double y, double Tx, double Ty, t_matrix *mat)
 {
   printf("%.3g %.3g\n",mat->x, mat->y);
   printf("Homothety by the ratios %.3g and %.3g\n", Tx, Ty);
-  mat->tab[0][0] += Tx;
-  mat->tab[0][1] += 0.000;
-  mat->tab[0][2] += 0.000;
-  mat->tab[1][0] += 0.000;
-  mat->tab[1][1] += Ty;
-  mat->tab[1][2] += 1.000;
+  mat->tab[0][0] *= Tx;
+  mat->tab[0][1] *= 0.000;
+  mat->tab[0][2] *= 0.000;
+  mat->tab[1][0] *= 0.000;
+  mat->tab[1][1] *= Ty;
+  mat->tab[1][2] *= 1.000;
   mat->x = Tx * mat->x;
   mat->y = Ty * mat->y;
 }
@@ -61,12 +61,12 @@ void    case_r(double x, double y, double A, double buffer, t_matrix *mat)
   if (d < 0.001 && d > 0.000)
     d = 0;
   printf("Rotation at a %.3g degree angle\n", A);
-  mat->tab[0][0] += a;
-  mat->tab[0][1] += b;
-  mat->tab[0][2] += 0.000;
-  mat->tab[1][0] += c;
-  mat->tab[1][1] += d;
-  mat->tab[1][2] += 0.000;
+  mat->tab[0][0] *= a;
+  mat->tab[0][1] *= b;
+  mat->tab[0][2] *= 0.000;
+  mat->tab[1][0] *= c;
+  mat->tab[1][1] *= d;
+  mat->tab[1][2] *= 0.000;
   mat->x = a * mat->x + b * mat->y;
   mat->y = c * mat->x + d * mat->y;
 }
@@ -88,12 +88,12 @@ void    case_s(double x, double y, double A, double buffer, t_matrix *mat)
   if (d < 0.001 && d > 0.000)
     d = 0;
   printf("Symmetry about an axis inclined with an angle of %.3g degrees\n", A);
-  mat->tab[0][0] += a;
-  mat->tab[0][1] += b;
-  mat->tab[0][2] += 0.000;
-  mat->tab[1][0] += c;
-  mat->tab[1][1] += d;
-  mat->tab[1][2] += 0.000;
+  mat->tab[0][0] *= a;
+  mat->tab[0][1] *= b;
+  mat->tab[0][2] *= 0.000;
+  mat->tab[1][0] *= c;
+  mat->tab[1][1] *= d;
+  mat->tab[1][2] *= 0.000;
   mat->x = a * mat->x + b * mat->y;
   mat->y = c * mat->x + d * mat->y;
 }
